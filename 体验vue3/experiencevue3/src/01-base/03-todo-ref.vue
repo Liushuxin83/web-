853fb1531972fa73c21673b12f8045ad412174ec
liushuxin <1443634341@qq.com>
2021-04-28 22:10:32 +0800

    ！

---
+++ 体验vue3/experiencevue3/src/01-base/03-todo-ref.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" ref="myText" />
+    <button @click="onAdd">Add</button>
+    <ul>
+      <li v-for="(item, index) in state.dataList" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  name: 'TodoRef',
+  setup () {
+    const state = reactive({
+      dataList: []
+    })
+    const myText = ref('')
+    const onAdd = () => {
+      // console.log(myText.value.value)
+      state.dataList.push(myText.value.value)
+      myText.value.value = ''
+    }
+    return {
+      myText,
+      onAdd,
+      state
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
