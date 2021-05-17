86f6721a515b27a5c5a60da6fc32ec364dc39f6f
liushuxin <1443634341@qq.com>
2021-05-17 12:39:30 +0800

    体验Vue3

---
+++ 体验vue3/experiencevue3/src/01-base/14-自定义hooks函数.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    自定义hooks函数，当用户点击页面后，获取点击的横纵坐标位置，并显示出来
+  </div>
+  <h2>x: {{ x }}---y: {{ y }}</h2>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import useMousePosition from '../01-base/hooks/useMousePosition'
+export default defineComponent({
+  setup () {
+    const { x, y } = useMousePosition()
+    return {
+      x,
+      y
+    }
+  }
+})
+</script>
---
+++ 体验vue3/experiencevue3/src/01-base/15-toRefs.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <h2>name: {{ state.name }}</h2>
+  <h2>name: {{ state.age }}</h2> -->
+  <h2>name: {{ name }}</h2>
+  <h2>name: {{ age }}</h2>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const state = reactive({
+      name: '刘舒新',
+      age: 18
+    })
+    // 定时器 更新数据，如果数据变化了，界面也会随之变化肯定是响应式数据
+    setInterval(() => {
+      state.name += '==='
+    }, 1000)
+    return {
+      ...toRefs(state)
+    }
+  }
+})
+</script>
