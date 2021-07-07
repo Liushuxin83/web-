4e039f18f5e94b506fcc2c603dcf52bb7bc2ce27
liushuxin <1443634341@qq.com>
2021-07-07 16:26:04 +0800

    vue3组件上使用v-model

---
+++ 体验vue3/experiencevue3/src/01-base/22-组件上使用v-model.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>父组件---{{ data }}</div>
+  <hr />
+  <!-- 一般情况下子传父 -->
+  <!-- <VmodelCompChild :data="data" @changeParentProps="changeProps" /> -->
+  <VmodelCompChild v-model="data" />
+  <!-- 如果不想叫modelValue这个名字  可以写成   v-model:abc="data"   !!!!-->
+  <!-- 等价于 -->
+  <!-- <VmodelCompChild :modelValue="data" @update:modelValue="data = $event" /> -->
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import VmodelCompChild from './VmodelCompChild.vue'
+
+export default defineComponent({
+  setup () {
+    const data = ref('刘舒新')
+    return {
+      data
+    }
+  },
+  components: {
+    VmodelCompChild
+  }
+})
+</script>
---
+++ 体验vue3/experiencevue3/src/01-base/VmodelCompChild.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    子组件
+  </div>
+  <button @click="changeParentProps">更改父组件传过来的props</button>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  emits: ['update:modelValue'],
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props, { emit }) {
+    // console.log(props)
+    const changeParentProps = () => {
+      emit('update:modelValue', '123')
+    }
+    return {
+      changeParentProps
+    }
+  }
+})
+</script>
